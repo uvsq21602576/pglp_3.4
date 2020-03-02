98eb10349e32cae3b087651bf5b15d42ef2e5bb7
Flora Carré <flora.carre@ens.uvsq.fr>
2020-03-02 18:07:04 +0100

    SimplePrinter et interface Print

---
+++ src/main/java/fr/uvsq/uvsq21602576/pglp_3_4/Print.java
@@ -0,0 +1,5 @@
+package fr.uvsq.uvsq21602576.pglp_3_4;
+
+public interface Print {
+	public void print();
+}
---
+++ src/main/java/fr/uvsq/uvsq21602576/pglp_3_4/SimplePrinter.java
@@ -0,0 +1,12 @@
+package fr.uvsq.uvsq21602576.pglp_3_4;
+
+public class SimplePrinter implements Print {
+
+	public SimplePrinter() {}
+
+	public void print() {
+		
+	};
+	
+
+}
