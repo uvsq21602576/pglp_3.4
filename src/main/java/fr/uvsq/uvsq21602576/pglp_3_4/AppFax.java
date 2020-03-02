eef867251412c73891cc6f02f6c8bb653df40143
Flora Carré <flora.carre@ens.uvsq.fr>
2020-03-02 18:09:04 +0100

    Interface Fax avec classe AppFax

---
+++ src/main/java/fr/uvsq/uvsq21602576/pglp_3_4/AppFax.java
@@ -0,0 +1,17 @@
+package fr.uvsq.uvsq21602576.pglp_3_4;
+
+import java.util.List;
+
+import javax.swing.text.Document;
+
+public class AppFax implements Fax {
+
+	public AppFax() {
+		
+	}
+
+	public void fax(List<Document> l) {
+		
+	}
+
+}
---
+++ src/main/java/fr/uvsq/uvsq21602576/pglp_3_4/Fax.java
@@ -0,0 +1,9 @@
+package fr.uvsq.uvsq21602576.pglp_3_4;
+
+import java.util.List;
+
+import javax.swing.text.Document;
+
+public interface Fax {
+	public void fax(List<Document> l);
+}
